ff86c49b712b6daf58811c5423056abcc66bd998
Mauricio <mrcludovico@gmail.com>
2025-01-29 20:03:40 -0300

    testando deploy

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders Hello, World! text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Testando o deploy mais uma vez!/i);
+  const linkElement = getByText(/Testando o deploy!/i);
   expect(linkElement).toBeInTheDocument();
 });
