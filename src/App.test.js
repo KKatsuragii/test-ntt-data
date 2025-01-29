ba3122df8a50a02be3856172c71b7f7079dbd93e
Mauricio <mrcludovico@gmail.com>
2025-01-29 19:27:22 -0300

    edit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders Hello, World! text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, World!/i);
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hello, World!/i);
   expect(linkElement).toBeInTheDocument();
 });
